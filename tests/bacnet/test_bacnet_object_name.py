4397528930e56e525e38ae695a8c5b3632cd2d98
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-09-03 16:47:39 +0000

    Updated example to match local IP, ID.

--- tests/bacnet/test_bacnet_object_name.py
+++ tests/bacnet/test_bacnet_object_name.py
@@ -2,15 +2,20 @@ import asyncio
 
 import BAC0
 
-DISTECH_CONTROLLER = "192.168.1.100:47808"
+DISTECH_CONTROLLER = "10.1.1.78:47808"
 DISTECH_CONTROLLER_DEVICE_ID = 1000
 
+
 async def main():
-    bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER)
+    print("Setting up BAC0")
+    bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER, bbmdTTL=900)
     bacnet.discover(
-        limits=(DISTECH_CONTROLLER_DEVICE_ID, DISTECH_CONTROLLER_DEVICE_ID), global_broadcast=True
+        limits=(DISTECH_CONTROLLER_DEVICE_ID, DISTECH_CONTROLLER_DEVICE_ID),
+        global_broadcast=True,
     )
-    present_value = await bacnet.read(f"{DISTECH_CONTROLLER} analogValue {DISTECH_CONTROLLER_DEVICE_ID} objectList")
+    present_value = await bacnet.read(
+        f"{DISTECH_CONTROLLER} analogValue {DISTECH_CONTROLLER_DEVICE_ID} objectList"
+    )
     print(f"Present Value: {present_value}")
 
 
