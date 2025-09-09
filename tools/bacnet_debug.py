6c6889d2193a85daaa3f9f1be66f24065c9ac08e
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-09-09 14:54:06 +0000

    Added workaround for what looks like BAC0 bug

--- tools/bacnet_debug.py
+++ tools/bacnet_debug.py
@@ -1,6 +1,7 @@
 import asyncio
 
 import BAC0
+from BAC0.core.devices.Device import Device
 
 from distech.constants import DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID
 
@@ -9,10 +10,17 @@ async def main():
     print("Setting up BAC0")
     bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER, bbmdTTL=900)
     device = await BAC0.device(DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID, bacnet)
-    object_list = await bacnet.read(
-        f"{DISTECH_CONTROLLER} device {DISTECH_CONTROLLER_DEVICE_ID} objectList"
-    )
-    print(object_list)
+
+    get_points(device)
+
+
+def get_points(device: Device):
+    """Workaround for a bug in BAC0"""
+    point_count = len(device.points)
+    points = list()
+    for i in range(point_count):
+        points.append(device.points[i])
+    return points
 
 
 if __name__ == "__main__":
