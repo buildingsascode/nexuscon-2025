c6f7d59c6881813f0d99cf48ce68eda12ff2f664
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-09-10 23:14:21 +0000

    Added example write

--- tools/bacnet_on.py
+++ tools/bacnet_on.py
@@ -1,22 +1,13 @@
 import asyncio
 
 import BAC0
-from BAC0.core.devices import Points
 
-from distech.constants import DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID
+from distech.constants import DISTECH_CONTROLLER
 
 
 async def main():
-    print("Setting up BAC0")
     bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER, bbmdTTL=900)
-    device = await BAC0.device(DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID, bacnet)
-    await asyncio.sleep(1)
-    points: list[Points.Point] = device.points
-    print(device.points)
-    for point in points:
-        print(
-            f"Point: {point.properties.name=}, {point.tags=}, {point.units=}, {await point.value=}, {point.status=}, {point.properties.description=}, {point.properties.type=}"
-        )
+    await bacnet._write(f"{DISTECH_CONTROLLER} binaryInput 101 presentValue active")
 
 
 if __name__ == "__main__":
