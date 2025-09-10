ad162448a2693eb8bc1d27ed8be2c2074a323728
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-09-10 23:26:03 +0000

    Fixed conversion from BAC0 type to template type.

--- tests/bacnet/test_bacnet_object_name.py
+++ tests/bacnet/test_bacnet_object_name.py
@@ -1,11 +1,15 @@
+import asyncio
+
 import BAC0
 import pytest
 import pytest_asyncio
 from BAC0.core.devices.Points import Point
 
 from distech.constants import DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID
-from tools.templates.points import PointTemplate
+from tools.templates.points import BACnetObjectType, PointTemplate
 from tools.templates.templates import get_template_points
+
+BAC0.log_level("silence")
 
 
 @pytest.fixture(scope="module")
@@ -14,7 +18,9 @@ def bacnet_template_points() -> list[PointTemplate]:
 
 
 @pytest.fixture(scope="module")
-def bacnet_template_points_by_name(bacnet_template_points) -> dict[str, PointTemplate]:
+def bacnet_template_points_by_name(
+    bacnet_template_points: list[PointTemplate],
+) -> dict[str, PointTemplate]:
     return {point.name: point for point in bacnet_template_points}
 
 
@@ -22,13 +28,15 @@ def bacnet_template_points_by_name(bacnet_template_points) -> dict[str, PointTem
 async def bacnet_device_points() -> list[Point]:
     bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER, bbmdTTL=900)
     device = await BAC0.device(DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID, bacnet)
+    await asyncio.sleep(1)
     points = device.points
     return points
 
 
 @pytest.mark.asyncio
 async def test_controller_point_names_in_template(
-    bacnet_template_points_by_name, bacnet_device_points
+    bacnet_template_points_by_name,
+    bacnet_device_points,
 ):
     for point in bacnet_device_points:
         assert (
@@ -38,7 +46,8 @@ async def test_controller_point_names_in_template(
 
 @pytest.mark.asyncio
 async def test_template_points_in_controller(
-    bacnet_template_points_by_name, bacnet_device_points
+    bacnet_template_points_by_name: dict[str, PointTemplate],
+    bacnet_device_points: list[Point],
 ):
     for point_name in bacnet_template_points_by_name:
         assert any(
@@ -47,4 +56,15 @@ async def test_template_points_in_controller(
 
 
 @pytest.mark.asyncio
-async def test_point_units(bacnet_template_points_by_name): ...
+async def test_point_type(
+    bacnet_template_points_by_name: dict[str, PointTemplate],
+    bacnet_device_points: list[Point],
+):
+    for point in bacnet_device_points:
+        assert (
+            point.properties.name is not None
+        ), "Unable to test point type, point name is None"
+        template_point = bacnet_template_points_by_name[point.properties.name]
+        assert template_point.type == BACnetObjectType.from_bac0_type(
+            point.properties.type
+        ), f"Point type mismatch for point {point.properties.name}: template type {template_point.type}, controller type {point.properties.type}"
