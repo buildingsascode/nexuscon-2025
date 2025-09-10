dd2d14a734f27a9e87f5fd4c9adb1da575d58f1b
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-09-10 21:15:42 +0000

    Added test to ensure template points are implemented.

--- tests/bacnet/test_bacnet_object_name.py
+++ tests/bacnet/test_bacnet_object_name.py
@@ -27,11 +27,23 @@ async def bacnet_device_points() -> list[Point]:
 
 
 @pytest.mark.asyncio
-async def test_point_names(bacnet_template_points_by_name, bacnet_device_points):
+async def test_controller_point_names_in_template(
+    bacnet_template_points_by_name, bacnet_device_points
+):
     for point in bacnet_device_points:
         assert (
             point.properties.name in bacnet_template_points_by_name
         ), f"Unexpected point name: {point.properties.name}"
+
+
+@pytest.mark.asyncio
+async def test_template_points_in_controller(
+    bacnet_template_points_by_name, bacnet_device_points
+):
+    for point_name in bacnet_template_points_by_name:
+        assert any(
+            point.properties.name == point_name for point in bacnet_device_points
+        ), f"Point {point_name} from template not found in controller points"
 
 
 @pytest.mark.asyncio
