7f3d49dcb813bbb0f970e637681d884d0c35f005
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-09-10 13:58:06 +0000

    Basic enumeration of point properties.

--- tools/bacnet_debug.py
+++ tools/bacnet_debug.py
@@ -1,30 +1,22 @@
 import asyncio
 
 import BAC0
-from bacpypes3.primitivedata import ObjectIdentifier
+from BAC0.core.devices import Points
 
 from distech.constants import DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID
 
-
-async def get_object_list(bacnet, device_id) -> list:
-    bacnet_objects = await bacnet.read(
-        f"{DISTECH_CONTROLLER} device {device_id} objectList"
-    )
-    return bacnet_objects
-
-
 async def main():
     print("Setting up BAC0")
-    BAC0.log_level("silence")
     bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER, bbmdTTL=900)
     device = await BAC0.device(DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID, bacnet)
-    bacnet_objects: list[ObjectIdentifier] = await get_object_list(
-        bacnet, DISTECH_CONTROLLER_DEVICE_ID
-    )
-
-    for bacnet_object in bacnet_objects:
-        print(await bacnet.read(f"{DISTECH_CONTROLLER} {bacnet_object} objectName"))
+    await asyncio.sleep(1)
+    points: list[Points.Point] = device.points
+    print(device.points)
+    for point in points:
+        print(f"Point: {point.auto=}, {point.tags=}, {point.units=}, {await point.value=}, {point.status=}")
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
