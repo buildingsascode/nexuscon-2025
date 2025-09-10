d62f4047f7d236a18cb0982d3fb7bb74728c9a0b
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-09-10 21:11:30 +0000

    Setup controller points as fixtures.

--- tests/bacnet/test_bacnet_object_name.py
+++ tests/bacnet/test_bacnet_object_name.py
@@ -1,5 +1,6 @@
 import BAC0
 import pytest
+import pytest_asyncio
 from BAC0.core.devices.Points import Point
 
 from distech.constants import DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID
@@ -13,17 +14,25 @@ def bacnet_template_points() -> list[PointTemplate]:
 
 
 @pytest.fixture(scope="module")
-def bacnet_template_points_by_name(bacnet_points) -> dict[str, PointTemplate]:
-    return {point.name: point for point in bacnet_points}
+def bacnet_template_points_by_name(bacnet_template_points) -> dict[str, PointTemplate]:
+    return {point.name: point for point in bacnet_template_points}
+
+
+@pytest_asyncio.fixture(scope="module")
+async def bacnet_device_points() -> list[Point]:
+    bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER, bbmdTTL=900)
+    device = await BAC0.device(DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID, bacnet)
+    points = device.points
+    return points
 
 
 @pytest.mark.asyncio
-async def test_point_names(bacnet_template_points_by_name):
-    print("Setting up BAC0")
-    bacnet = BAC0.lite(bbmdAddress=DISTECH_CONTROLLER, bbmdTTL=900)
-    device = await BAC0.device(DISTECH_CONTROLLER, DISTECH_CONTROLLER_DEVICE_ID, bacnet)
-    points: list[Point] = device.points
-    for point in points:
+async def test_point_names(bacnet_template_points_by_name, bacnet_device_points):
+    for point in bacnet_device_points:
         assert (
             point.properties.name in bacnet_template_points_by_name
         ), f"Unexpected point name: {point.properties.name}"
+
+
+@pytest.mark.asyncio
+async def test_point_units(bacnet_template_points_by_name): ...
