7ad27d19c6507af12bd1ec611eca225670e7d628
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-10-03 02:42:03 +0000

    Updated reads writes.

--- tools/bacnet_read.py
+++ tools/bacnet_read.py
@@ -6,7 +6,10 @@ import BAC0
 
 async def main():
     bacnet = BAC0.lite(bbmdAddress=os.environ["BBMD_IP"], bbmdTTL=900)
-    await bacnet._write(f"{os.environ["DISTECH_DEVICE"]} binaryInput 101 presentValue active")
+    switch_input = await bacnet.read(
+        f"{os.environ['DISTECH_DEVICE']} binaryInput 101 presentValue"
+    )
+    print(f"Switch Input: {switch_input}")
 
 
 if __name__ == "__main__":
--- tools/bacnet_write.py
+++ tools/bacnet_write.py
@@ -6,7 +6,9 @@ import BAC0
 
 async def main():
     bacnet = BAC0.lite(bbmdAddress=os.environ["BBMD_IP"], bbmdTTL=900)
-    await bacnet._write(f"{os.environ["DISTECH_DEVICE"]} binaryInput 101 presentValue active")
+    await bacnet._write(
+        f"{os.environ["DISTECH_DEVICE"]} binaryInput 101 presentValue active"
+    )
 
 
 if __name__ == "__main__":
