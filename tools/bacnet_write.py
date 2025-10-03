92a65858826c2a8ce107a4ab1b634f1eca60ee84
Douglas Plumley <douglas.plumley@dartmouth.edu>
2025-10-03 02:38:56 +0000

    Setting up debugging for reads.

---
+++ tools/bacnet_write.py
@@ -0,0 +1,13 @@
+import asyncio
+import os
+
+import BAC0
+
+
+async def main():
+    bacnet = BAC0.lite(bbmdAddress=os.environ["BBMD_IP"], bbmdTTL=900)
+    await bacnet._write(f"{os.environ["DISTECH_DEVICE"]} binaryInput 101 presentValue active")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
